6552af1d91f5ba35e2770f979208a2868571fc3a
sergey <mac@MacBook-Air-sergey.local>
2026-02-11 16:46:57 +0300

    moved the flatten function out for optimization

--- source/plainify.js
+++ source/plainify.js
@@ -8,26 +8,27 @@
  */
 function plainify(obj) {
     const result = {};
-    /**
-     * Внутренняя рекурсивная функция для обхода объекта
-     * 
-     * @param {Object} currentObj - Текущий объект для обработки
-     * @param {string} parentKey - Префикс для формирования составных ключей
-     * @returns {void}
-     */
-    function flatten(currentObj, parentKey) {
-        Object.keys(currentObj).forEach(key => {
-            const newKey = parentKey ? `${parentKey}.${key}` : key;
-            const value = currentObj[key];
-            
-            if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
-                flatten(value, newKey);
-            } else {
-                result[newKey] = value;
-            }
-        });
-    }
-
-    flatten(obj, '');
+    flatten(obj, '', result);
     return result;
+}
+
+/**
+* рекурсивная функция для обхода объекта
+* 
+* @param {Object} currentObj - Текущий объект для обработки
+* @param {string} parentKey - Префикс для формирования составных ключей
+* @param {Object} result - Аккумулятор результатов
+* @returns {void}
+*/
+function flatten(currentObj, parentKey, result) {
+    Object.keys(currentObj).forEach(key => {
+        const newKey = parentKey ? `${parentKey}.${key}` : key;
+        const value = currentObj[key];
+            
+        if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+            flatten(value, newKey, result);
+        } else {
+            result[newKey] = value;
+        }
+    });
 }
