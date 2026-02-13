ed103506626b7d67335869ec91a449018a86c7a0
sergey <mac@MacBook-Air-sergey.local>
2026-02-13 14:28:52 +0300

    rewrote flatten function to arrow function

--- source/plainify.js
+++ source/plainify.js
@@ -1,26 +1,13 @@
 'use strict'
 /**
- * Рекурсивно преобразует вложенный объект в плоский объект, где составные ключи формируются через точку
- * 
- * @param {Object} obj - Исходный объект для преобразования
- * @returns {Object} Плоский объект с ключами-путями
- * 
- */
-function plainify(obj) {
-    const result = {};
-    flatten(obj, '', result);
-    return result;
-}
-
-/**
-* рекурсивная функция для обхода объекта
+* рекурсивная стрелочная функция для обхода объекта
 * 
 * @param {Object} currentObj - Текущий объект для обработки
 * @param {string} parentKey - Префикс для формирования составных ключей
 * @param {Object} result - Аккумулятор результатов
 * @returns {void}
 */
-function flatten(currentObj, parentKey, result) {
+const flatten = (currentObj, parentKey, result) => {
     Object.keys(currentObj).forEach(key => {
         const newKey = parentKey ? `${parentKey}.${key}` : key;
         const value = currentObj[key];
@@ -31,4 +18,17 @@ function flatten(currentObj, parentKey, result) {
             result[newKey] = value;
         }
     });
+};
+
+/**
+ * Рекурсивно преобразует вложенный объект в плоский объект, где составные ключи формируются через точку
+ * 
+ * @param {Object} obj - Исходный объект для преобразования
+ * @returns {Object} Плоский объект с ключами-путями
+ * 
+ */
+function plainify(obj) {
+    const result = {};
+    flatten(obj, '', result);
+    return result;
 }
